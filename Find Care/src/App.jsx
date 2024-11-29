45d8fad52668dab8c6629d93ab6383991b4905d1
Mohammed Faran <er.faran23@gmail.com>
2024-11-29 10:13:53 +0530

    FEAT: Navbar placed common place

--- Find Care/src/App.jsx
+++ Find Care/src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Member/Components/Home.jsx";
+import AdminDashboard from "./Admin/Components/AdminDashboard.jsx";
+import NavbarComponent from "./Common/components/NavbarComponent.jsx";
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <NavbarComponent />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
