e47419a134fb9cb5fc9c2bef431eec3fee3ec352
Jaibharath159 <bharathpabolu159@gmail.com>
2024-11-29 09:00:14 +0530

    FET 29-11-2024

--- Find Care/src/App.jsx
+++ Find Care/src/App.jsx
@@ -1,9 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Member/Components/Home.jsx";
-import NavbarComponent from "./Common/components/NavbarComponent.jsx";
-import BannerComponent from "./Member/Components/BannerComponent.jsx";
-
 function App() {
   return (
     <Router>
