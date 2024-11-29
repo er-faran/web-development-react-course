6f49c30a396a3e33982af47f41d103b488ff7c0c
renukack <renuani181@gmail.com>
2024-11-28 22:35:09 -0500

    Footer component

--- Find Care/src/App.jsx
+++ Find Care/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import BannerComponent from "./components/BannerComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
   return (
     <>
       {/* <h1 className="text-3xl bg-gray-600">Find Care</h1> */}
       <BannerComponent />
+      <FooterComponent />
     </>
   );
 }
