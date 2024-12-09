718e5244cf403b18bbf2719052531d70869ab489
Mohammed Faran <er.faran23@gmail.com>
2024-12-09 10:51:17 +0530

    FEAT : Navbar Loggedin user component added

--- Find Care/src/Common/components/NavbarComponent.jsx
+++ Find Care/src/Common/components/NavbarComponent.jsx
@@ -1,4 +1,15 @@
-import { Avatar } from "@mui/material";
+import {
+  Avatar,
+  Divider,
+  IconButton,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+
+import Logout from "@mui/icons-material/Logout";
+import MedicationIcon from "@mui/icons-material/Medication";
+
 import { useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 
@@ -16,6 +27,15 @@ const NavbarComponent = () => {
   if (adminRoutes.includes(location.pathname)) {
     isAdminNavbar = true;
   }
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <nav
@@ -136,7 +156,71 @@ const NavbarComponent = () => {
                   </NavLink>
                 ) : (
                   <>
-                    <Avatar> MF</Avatar>
+                    <IconButton
+                      onClick={handleClick}
+                      size="small"
+                      sx={{ ml: 2 }}
+                      aria-controls={open ? "account-menu" : undefined}
+                      aria-haspopup="true"
+                      aria-expanded={open ? "true" : undefined}
+                    >
+                      <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+                    </IconButton>
+
+                    <Menu
+                      anchorEl={anchorEl}
+                      id="account-menu"
+                      open={open}
+                      onClose={handleClose}
+                      onClick={handleClose}
+                      slotProps={{
+                        paper: {
+                          elevation: 0,
+                          sx: {
+                            overflow: "visible",
+                            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                            mt: 1.5,
+                            "& .MuiAvatar-root": {
+                              width: 32,
+                              height: 32,
+                              ml: -0.5,
+                              mr: 1,
+                            },
+                            "&::before": {
+                              content: '""',
+                              display: "block",
+                              position: "absolute",
+                              top: 0,
+                              right: 14,
+                              width: 10,
+                              height: 10,
+                              bgcolor: "background.paper",
+                              transform: "translateY(-50%) rotate(45deg)",
+                              zIndex: 0,
+                            },
+                          },
+                        },
+                      }}
+                      transformOrigin={{ horizontal: "right", vertical: "top" }}
+                      anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+                    >
+                      <MenuItem onClick={handleClose}>
+                        <Avatar /> My Profile
+                      </MenuItem>
+                      <Divider />
+                      <MenuItem onClick={handleClose}>
+                        <ListItemIcon>
+                          <MedicationIcon fontSize="small" />
+                        </ListItemIcon>
+                        My Appointments
+                      </MenuItem>
+                      <MenuItem onClick={handleClose}>
+                        <ListItemIcon>
+                          <Logout fontSize="small" />
+                        </ListItemIcon>
+                        Logout
+                      </MenuItem>
+                    </Menu>
                   </>
                 ))}
             </li>
