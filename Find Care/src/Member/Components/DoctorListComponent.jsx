543bfc92828e40ad5539c98b8fe351e73eb32cbc
Mohammed Faran <er.faran23@gmail.com>
2024-12-11 09:56:53 +0530

    Doctor List and Doctor Details page done

--- Find Care/src/Member/Components/DoctorListComponent.jsx
+++ Find Care/src/Member/Components/DoctorListComponent.jsx
@@ -1,17 +1,13 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 import DoctorCardComponent from "./DoctorCardComponent";
-import { useNavigate,useLocation } from "react-router-dom";
-
+import { useNavigate, useLocation } from "react-router-dom";
+import { database } from "../../firebaseConfig";
+import { onValue, ref } from "firebase/database";
 
 const DoctorListComponent = ({
-  
-
-
-
   isHeaderShow = true,
   specialityFilter = "",
-  
 }) => {
   const navigate = useNavigate();
   let isAdminNavbar = false;
@@ -26,87 +22,126 @@ const DoctorListComponent = ({
   if (adminRoutes.includes(location.pathname)) {
     isAdminNavbar = true;
   }
-  const doctorListData = [
-    {
-      id: "1",
-      name: "Sam",
-      speciality: "General Physician",
-      status: "Available",
-      doctorImg:
-        "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
-    },
-    {
-      id: "2",
-      name: "Sam 2",
-      speciality: "Gynecologist",
-      status: "Available",
-      doctorImg:
-        "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
-    },
-    {
-      id: "3",
-      name: "Sam 3",
-      speciality: "General Physician",
-      status: "Available",
-      doctorImg:
-        "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
-    },
-    {
-      id: "4",
-      name: "Sam 4",
-      speciality: "Gynecologist",
-      status: "Available",
-      doctorImg:
-        "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
-    },
-    {
-      id: "5",
-      name: "Sam 5",
-      speciality: "General Physician",
-      status: "Available",
-      doctorImg:
-        "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
-    },
-    {
-      id: "6",
-      name: "Sam 6",
-      speciality: "Gynecologist",
-      status: "Available",
-      doctorImg:
-        "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
-    },
-    {
-      id: "7",
-      name: "Sam 7",
-      speciality: "Neurologist",
-      status: "Available",
-      doctorImg:
-        "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
-    },
-    {
-      id: "8",
-      name: "Sam 8",
-      speciality: "Pediatricians",
-      status: "Available",
-      doctorImg:
-        "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
-    },
-    {
-      id: "9",
-      name: "Sam 9",
-      speciality: "Dermatologist",
-      status: "Available",
-      doctorImg:
-        "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
-    },
-  ];
+  // const doctorListData = [
+  //   {
+  //     id: "1",
+  //     name: "Sam",
+  //     speciality: "General Physician",
+  //     status: "Available",
+  //     doctorImg:
+  //       "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
+  //   },
+  //   {
+  //     id: "2",
+  //     name: "Sam 2",
+  //     speciality: "Gynecologist",
+  //     status: "Available",
+  //     doctorImg:
+  //       "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
+  //   },
+  //   {
+  //     id: "3",
+  //     name: "Sam 3",
+  //     speciality: "General Physician",
+  // status: "Available",
+  // doctorImg:
+  //   "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
+  //   },
+  //   {
+  //     id: "4",
+  //     name: "Sam 4",
+  //     speciality: "Gynecologist",
+  //     status: "Available",
+  //     doctorImg:
+  //       "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
+  //   },
+  //   {
+  //     id: "5",
+  //     name: "Sam 5",
+  //     speciality: "General Physician",
+  //     status: "Available",
+  //     doctorImg:
+  //       "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
+  //   },
+  //   {
+  //     id: "6",
+  //     name: "Sam 6",
+  //     speciality: "Gynecologist",
+  //     status: "Available",
+  //     doctorImg:
+  //       "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
+  //   },
+  //   {
+  //     id: "7",
+  //     name: "Sam 7",
+  //     speciality: "Neurologist",
+  //     status: "Available",
+  //     doctorImg:
+  //       "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
+  //   },
+  //   {
+  //     id: "8",
+  //     name: "Sam 8",
+  //     speciality: "Pediatricians",
+  //     status: "Available",
+  //     doctorImg:
+  //       "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
+  //   },
+  //   {
+  //     id: "9",
+  //     name: "Sam 9",
+  //     speciality: "Dermatologist",
+  //     status: "Available",
+  //     doctorImg:
+  //       "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
+  //   },
+  // ];
+
+  const [doctorListData, setDoctorListData] = useState([]);
+
+  const getAllDoctorList = () => {
+    try {
+      const starCountRef = ref(database, "doctors/");
+      onValue(
+        starCountRef,
+        (snapshot) => {
+          const dbData = snapshot.val();
+
+          const formattedData = [];
+
+          const dbDoctorData = Object.values(dbData);
+
+          dbDoctorData.map((item) => {
+            formattedData.push({
+              ...item,
+              status: "Available",
+              doctorImg:
+                "https://raw.githubusercontent.com/avinashdm/gs-images/main/prescripto/doc1.png",
+            });
+          });
+
+          setDoctorListData(formattedData);
+          setFilteredData(formattedData);
+        },
+        {
+          onlyOnce: true,
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const [filteredData, setFilteredData] = useState(doctorListData);
 
   useEffect(() => {
+    getAllDoctorList();
+  }, []);
+
+  useEffect(() => {
     if (specialityFilter !== "" && specialityFilter) {
-      const filterData = doctorListData.filter((doctor) => {
-        return doctor.speciality === specialityFilter;
+      const filterData = doctorListData?.filter((doctor) => {
+        return doctor?.speciality === specialityFilter;
       });
       setFilteredData(filterData);
     } else {
@@ -118,28 +153,37 @@ const DoctorListComponent = ({
     <div>
       <div className="text-center">
         {isHeaderShow && (
-          <div>{isAdminNavbar
-            ?<div className="justify-items-start"><h1 className="ml-6 py-4 text-lg font-medium">All Doctors</h1></div>
-            :<div>
-            <div>
-              <h2 className="section-heading">Top Doctors to Book</h2>
-            </div>
-            <div className="flex justify-center mb-5">
-              <p className="section-description">
-                Simply browse through our extensive list of trusted doctors.
-              </p>
-            </div>
-            </div>}
+          <div>
+            {isAdminNavbar ? (
+              <div className="justify-items-start">
+                <h1 className="ml-6 py-4 text-lg font-medium">All Doctors</h1>
+              </div>
+            ) : (
+              <div>
+                <div>
+                  <h2 className="section-heading">Top Doctors to Book</h2>
+                </div>
+                <div className="flex justify-center mb-5">
+                  <p className="section-description">
+                    Simply browse through our extensive list of trusted doctors.
+                  </p>
+                </div>
+              </div>
+            )}
           </div>
         )}
-        <div className={isAdminNavbar
-        ?"flex gap-5 justify-start ml-6 align-middle flex-wrap"
-        :"flex gap-5 justify-center align-middle flex-wrap"}>
+        <div
+          className={
+            isAdminNavbar
+              ? "flex gap-5 justify-start ml-6 align-middle flex-wrap"
+              : "flex gap-5 justify-center align-middle flex-wrap"
+          }
+        >
           {filteredData?.length === 0 ? (
             <p>No Doctor Available for the selected criteria.</p>
           ) : (
             filteredData?.map((doctor) => {
-              return <DoctorCardComponent key={doctor.id} data={doctor} />;
+              return <DoctorCardComponent key={doctor.email} data={doctor} />;
             })
           )}
         </div>
